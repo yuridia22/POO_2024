2fb8fc0ff43f8176a3229ea25758bd5ac41681be
Danachin554 <158262044+Danachin554@users.noreply.github.com>
2024-02-26 11:14:07 -0600

    DanaChin

---
+++ PRO/Beecrowd/AreaDelCirculo.java
@@ -0,0 +1,12 @@
+package DanaChin.Beecrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AreaDelCirculo {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        double R = scanner.nextDouble();
+        System.out.printf("A=%.4f\n", (3.14159 * R * R));
+    }
+}
