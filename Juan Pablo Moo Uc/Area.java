ecc6c57866c0d2ae7ca28dea6fe323626e2949ec
Juan Pablo Moo Uc <133072014+Cizco-2005@users.noreply.github.com>
2024-02-26 16:46:47 +0000

    MisCambiosJuan

---
+++ Juan Pablo Moo Uc/Area.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+ import java.util.Scanner;
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void Area (String[] args) throws IOException {
+ 
+      Scanner scanner = new Scanner(System.in);
+     
+      
+      
+      System.out.printf("A=%.4f%n",3.14159 * Math.pow(scanner.nextDouble(),2));
+        
+ 
+    }
+ 
+}
