dcd5d15ee95639c1b9f16ab5a5840120838a6126
ypech-itescam <ypech@itescam.edu.mx>
2024-02-29 13:05:01 -0600

    cambios

--- Yaqui/Ejercicios/Main.java
+++ Yaqui/Ejercicios/Main.java
@@ -1,15 +1,11 @@
 package Yaqui.Ejercicios;
 
-import ManuelYah.Clima;
-
 public class Main {
     public static void main(String[] arg) {
         Perro miperro = new Perro("Pedro");
         miperro.ladrar();
         System.out.println(miperro.getNombre());
         miperro.comer("la tarea Jeffrey");
-        Clima miclima = new Clima("lg", "verde", 32, 27, 34, true);
-        miclima.subirTemperatura(24);
-        System.out.println(miclima.getTemperatura());
+
     }
 }
