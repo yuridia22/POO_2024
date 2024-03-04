941c6408d119251f184fcc8fbe8b4211542c2f8f
EmanuelCauichCanul <emanuelcauich048@gmail.com>
2024-03-03 19:46:51 -0600

    miscambios

--- EmanuelCauich---8971/TAREAS DE CLASE/TAREA 5/Hoja.java
+++ EmanuelCauich---8971/TAREAS DE CLASE/TAREA 5/Hoja.java
@@ -1,3 +1,92 @@
+import java.time.LocalDate;
+
 public class Hoja {
-    
+    private LocalDate fecha;
+    private String tipo;
+    private String texto;
+    private LocalDate fechaLimite;
+    private String autor;
+    private String color;
+
+    public Hoja(int año, int mes, int dia, String tipo, String texto, String autor, int año2, int mes2, int dia2, String color) {
+        setFecha(LocalDate.of(año, mes, dia));
+        setTipo(tipo);
+        setTexto(texto);
+        setFechaLimite(LocalDate.of(año2, mes2, dia2));
+        setAutor(autor);
+        setColor(color);
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDate getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public void setFechaLimite(LocalDate fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        switch (color.toLowerCase()) {
+            case "azul":
+                this.color = "\u001B[34m"; // Código ANSI para azul
+                break;
+            case "amarillo":
+                this.color = "\u001B[33m"; // Código ANSI para amarillo
+                break;
+            case "rojo":
+                this.color = "\u001B[31m"; // Código ANSI para rojo
+                break;
+            default:
+                this.color = "\u001B[30m"; // Código ANSI para negro (por defecto)
+                break;
+        }
+    }
+
+    public void imprimir() {
+        String borde = "\u001B[38;5;" + color + "*********************************************************************\u001B[0m";
+        System.out.println(borde);
+        System.out.println("\033[38;5;" + color + "                                                           " + fecha);
+        System.out.println("\033[38;5;" + color + "Tipo == " + tipo);
+        System.out.println("\033[38;5;" + color + texto);
+        System.out.println("\033[38;5;" + color + "Fecha limite: ");
+        System.out.println("\033[38;5;" + color + "         "+ fechaLimite);
+        System.out.println("\033[38;5;" + color + "Autor = " + autor);
+        System.out.println(borde);
+    }
 }
--- EmanuelCauich---8971/TAREAS DE CLASE/TAREA 5/MainNota.java
+++ EmanuelCauich---8971/TAREAS DE CLASE/TAREA 5/MainNota.java
@@ -1,3 +1,11 @@
 public class MainNota {
-    
+    public static void main(String[] args) {
+
+        Hoja hoja = new Hoja(2024, 02,03, "urgente", "tarea si", "Emanuel", 2024, 02, 21, "azul");
+
+        hoja.imprimir();
+
+
+    }
 }
+
