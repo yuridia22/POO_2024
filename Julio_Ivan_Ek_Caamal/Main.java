58a088b2e0c5e280d5571673c459f0729837b4e1
Julioek8866 <julioivanekcaamal3432@gmail.com>
2024-03-06 08:58:12 -0600

    add: Styles

--- Julio_Ivan_Ek_Caamal/Main.java
+++ Julio_Ivan_Ek_Caamal/Main.java
@@ -1,11 +1,11 @@
 
-
+import Julio_Ivan_Ek_Caamal ANIMAL.Perro;
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
-       
+       Perro p = new ANIMAL.Perro("chapa", "doberman", "negro", "macho", 2, 34);
     }
 }
