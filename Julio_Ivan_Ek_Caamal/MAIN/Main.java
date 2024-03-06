d8a99017120e96f8ca5f5e442e94f78947da5858
Julioek8866 <julioivanekcaamal3432@gmail.com>
2024-03-05 23:36:37 -0600

    add:Styles

--- Julio_Ivan_Ek_Caamal/MAIN/Main.java
+++ Julio_Ivan_Ek_Caamal/MAIN/Main.java
@@ -1,11 +1,15 @@
 package MAIN;
 
+import Julio_Ivan_Ek_Caamal.ANIMAL.Perro;
+import Julio_Ivan_Ek_Caamal.TERMOMETRO_DIGITAL.TermometroDigital;
+
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
-      
+        Perro p = new Perro("chapa", 2, "doberman");
+        System.out.print(p.getNombre());      
     }
 }
