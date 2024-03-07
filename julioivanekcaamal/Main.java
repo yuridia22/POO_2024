c37d4825f7ae808e74a2222458294256f2043922
Julioek8866 <julioivanekcaamal3432@gmail.com>
2024-03-06 20:35:05 -0600

    add: Styles

--- julioivanekcaamal/Main.java
+++ julioivanekcaamal/Main.java
@@ -8,7 +8,7 @@ import julioivanekcaamal.PERRO1.Perro1;
 public class Main {
 
     public static void main(String[] args) {
-       Perro1 julio = new Perro1("chpa")
+       Perro1 julio = new Perro1("chpa");
        System.out.println(julio.getNombre()); 
     }
 }
