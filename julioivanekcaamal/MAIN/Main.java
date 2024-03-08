fa6232b1c0b7f0ccff9fac79696d54e4e4397098
Julioek8866 <julioivanekcaamal3432@gmail.com>
2024-03-08 09:10:15 -0600

    add: Styles

--- julioivanekcaamal/MAIN/Main.java
+++ julioivanekcaamal/MAIN/Main.java
@@ -1,13 +1,17 @@
 package julioivanekcaamal.MAIN;
 
+import julioivanekcaamal.ANIMAL.Perro;
+import julioivanekcaamal.AVATAR.Avatar;
+import julioivanekcaamal.HOLAMUNDO.MiHolaMundo;
+import julioivanekcaamal.OBJETO.Calculadora;
 import julioivanekcaamal.PERRO1.Perro1;
-import julioivanekcaamal.HOLAMUNDO.MiHolaMundo;;
+import julioivanekcaamal.TERMOMETRO_DIGITAL.TermometroDigital;
 
 
 public class Main {
 
     public static void main(String[] args) {
        MiHolaMundo m = new MiHolaMundo("");
-       System.out.println(m.cambiarColor());
+       m.cambiarColor();
     }
 }
