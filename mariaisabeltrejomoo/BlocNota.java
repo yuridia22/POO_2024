f7765b75a9963bba8ff2df17cafec8066cfd0bdb
Isabel-Trejo <trejoisabel592@gmail.com>
2024-03-08 01:04:38 -0600

    cambio

--- mariaisabeltrejomoo/BlocNota.java
+++ mariaisabeltrejomoo/BlocNota.java
@@ -1,3 +1,26 @@
-public class BlocNota {
-    
+public class BlocNotas {
+    private Nota nota1;
+    private Nota nota2; 
+    private Color c;
+
+    public BlocNotas() {
+        nota1= new Nota("29/02/2024", "Terminar la practica 3", "01/03/2024");
+        nota2= new Nota("04/03/2024", "Empezar la practica 4", "08/03/2024");
+        c = new Color("magenta");
+    }
+
+    public Color getColor() {
+        return c;
+    }
+
+    public void setColor(String color) {
+        this.c.setColor(color);
+    }
+
+    public void mostrarNotas() {
+        System.out.println(c.getColorANSI() + "Bloc de notas:");
+        System.out.println(c.getColorANSI());
+        System.out.println(nota1);
+        System.out.println(nota2);
+    }
 }
