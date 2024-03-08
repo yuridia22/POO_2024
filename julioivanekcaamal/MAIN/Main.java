1fe2d30d8522a6c0651ddb8d9d65cbf30029d5f7
Julioek8866 <julioivanekcaamal3432@gmail.com>
2024-03-08 09:05:26 -0600

    add: Styles

--- julioivanekcaamal/MAIN/Main.java
+++ julioivanekcaamal/MAIN/Main.java
@@ -1,12 +1,13 @@
 package julioivanekcaamal.MAIN;
 
 import julioivanekcaamal.PERRO1.Perro1;
+import julioivanekcaamal.HOLAMUNDO.MiHolaMundo;;
 
 
 public class Main {
 
     public static void main(String[] args) {
-       Perro1 p = new Perro1("cjasbcj");
-       System.out.println(p.getNombre()); 
+       MiHolaMundo m = new MiHolaMundo("");
+       System.out.println(m.cambiarColor());
     }
 }
